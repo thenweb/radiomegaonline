0820a0dcf47084933e692dde7a28a9a473d6b9d4
raiweb <rafaelvargas1164@gmail.com>
2026-09-15 01:26:26 -0400

    java

--- js/main.js
+++ js/main.js
@@ -1,36 +1,78 @@
 const typed = new Typed('.typed', {
 	strings: [
+	    '<i class="typed"> ^1000', // Pausa
 		'<i class="typed">Así que la fe es por el oír, </i>',
 		'<i class="typed">y el oír, por la palabra de Dios. </i>',
 		'<i class="typed">Romanos 10:17. </i>',
-        
-		'<i class="typed">Jesús le respondió: Escrito está: </i>',
-		'<i class="typed">“No solo de pan vive el hombre, </i>',
-		'<i class="typed">sino de toda palabra que sale </i>',
-		'<i class="typed">de la boca de Dios” </i>',
-        '<i class="typed">Mateo 4:4. </i>',
-        
-		'<i class="typed">Mira que estoy a la puerta y llamo. </i>',
-		'<i class="typed">Si alguno oye mi voz y abre la puerta, </i>',
-		'<i class="typed">entraré, y cenaré con él, y él conmigo.</i>',
-		'<i class="typed">Apocalipsis 3:20. </i>',
-        
+        		        
 		'<i class="typed">EL que oye mi palabra, </i>',
         '<i class="typed">y cree al que me envió, tiene vida eterna </i>',
         '<i class="typed">Juan 5:24. </i>',
+		'<i class="typed"> ^2000', // Pausa 
 		
+		// AQUI LOS PROGRAMAS EN VIVO
+		// LUNES _____________________________
+		'<i class="typed" style="color: #ff0000;">PROGRAMAS EN VIVO: </i>',
+        '<i class="typed" style="color: #076aff;">"Primer Contacto" </i>',
+		'<i class="typed">(Lunes, Miércoles y Viernes) </i>',
+		'<i class="typed">De 5am a 7am </i>',
+		'<i class="typed"> ^1000', // Pausa
 		
+		'<i class="typed" style="color: #179d23;">"La Voz Fraternal" </i>',
+		'<i class="typed">(Lunes de 3pm a 5pm) </i>',
+		'<i class="typed"> ^1000', // Pausa
+		//____________________________________
+		
+		// MARTES _____________________________
+		'<i class="typed" style="color: #8172f8;">"Impacto de Dios" </i>',
+		'<i class="typed">(Martes y Jueves de 3pm a 5pm) </i>',
+		'<i class="typed"> ^1000', // Pausa
+		//____________________________________
+		
+		// MIERCOLES _____________________________
+		'<i class="typed" style="color: #59a8be;">"Jóvenes con Propósito" </i>',
+		'<i class="typed">(Miércoles de 4pm a 6pm) </i>',
+		'<i class="typed"> ^1000', // Pausa
+		//____________________________________
+		
+		// JUEVES _____________________________
+		'<i class="typed" style="color: #d50a4d;">"Buscando la Senda Antigua" </i>',
+		'<i class="typed">(Jueves de 10am a 12m) </i>',
+		'<i class="typed"> ^1000', // Pausa
+		//____________________________________
+		
+		// VIERNES _____________________________
+		'<i class="typed" style="color: #179d23;">"La Palabra de Dios" </i>',
+		'<i class="typed">(Lunes y Viernes de 10am a 12m) </i>',
+		'<i class="typed"> ^1000', // Pausa
+		//____________________________________
+		
+		// SABADO
+        '<i class="typed" style="color: #00f5fe;">"Milagros del Cielo" </i>',
+		'<i class="typed">(Sábado de 10am a 12m) </i>',
+		'<i class="typed"> ^1000', // Pausa
+		//____________________________________
+		
+        // DOMINGO _____________________________		
+		'<i class="typed" style="color: #a9933c;">"Apasionados por Cristo" </i>',
+		'<i class="typed">(Domingo de 4pm a 6pm) </i>',
+		'<i class="typed"> ^1000', // Pausa
+		//____________________________________
+		
+		//____________________________________
+		'<i class="typed" style="color: #ffd800;">"Conéctate y Reporta tu Sintonía"</i>',
+		'<i class="typed"> ^1000', // Pausa
 		
 	],
 
 	 
 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 60, // Velocidad en mlisegundos para poner una letra,
+	typeSpeed: 50, // Velocidad en mlisegundos para poner una letra,
 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
 	backSpeed: 25, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	backDelay: 2000, // Tiempo de espera despues de que termina de escribir una palabra.
 	loop: true, // Repetir el array de strings
 	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
 	showCursor: true, // Mostrar cursor palpitanto
